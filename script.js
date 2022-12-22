775e406371b13f0bd5607a6eea89b28c841f038c
sibi71 <thearunsibi@gmail.com>
2022-12-22 09:41:41 +0530

    big fix

--- script.js
+++ script.js
@@ -12,49 +12,57 @@ async function getapi(url){
     const res = await fetch(url);
      const data = await res.json();
       aldata = data;
-   
-        getpagenumber(aldata);
-
-   
-
+      getpagenumber();
+        
 }
- 
 getapi(api_URL);
 
+    function getpagenumber(){
+        let pageno = 0; 
+        let pages = aldata.length / 10;
+        for (let i= 0 ; i < pages; i++){
 
-
-async function getpagenumber(){
-    let pageno = 0; 
-    let pages = aldata.length / 10 ;
-    for (let i= 0 ; i < pages; i++){
-        let button = document.createElement("button");
-        button.innerHTML=`${i+1}`;
-        button.onclick = ()=>{
-            pageno = i;
-           
-            // getcontent();
-            content.innerHTML="";
-            let displayData = aldata.slice(pageno*10 , (pageno + 1)*10);
-            displayData.map((data)=>{
-                const tr = document.createElement("tr");
-                    tr.innerHTML=`
-                    <td id="dataid">${data.id}</td>
-                    <td >${data.email}</td>
-                    <td >${data.name}</td>`;
-                    
-                    content.appendChild(tr);
-            }) 
-          
-    };  
-
-    pagenumber.append(button);
+                let button = document.createElement("button");
+                button.innerHTML=`${i+1}`;
+                button.onclick = ()=>{
+                    pageno = i;
+                    getcontentnext(pageno);
+                };  
+                getcontentfirst(pageno);
+                pagenumber.append(button);
+        }
     }
+
+    function getcontentfirst(pageno) {
+        content.innerHTML="";
+        let displayData = aldata.slice(pageno*10 , (pageno + 1)*10);
+       
+        displayData.map((get)=>{
+            
+            const tr = document.createElement("tr");
+                tr.innerHTML=`
+                <td id="dataid">${get.id}</td>
+                <td >${get.email}</td>
+                <td >${get.name}</td>`;
+                content.appendChild(tr);
+        }) 
+    }
+
+ function getcontentnext(pageno) {
+    content.innerHTML="";
+    let displayData = aldata.slice(pageno*10 , (pageno + 1)*10);
+   
+    displayData.map((get)=>{
+        
+        const tr = document.createElement("tr");
+            tr.innerHTML=`
+            <td id="dataid">${get.id}</td>
+            <td >${get.email}</td>
+            <td >${get.name}</td>`;
+            content.appendChild(tr);
+    }) 
 }
 
-// async function getcontent() {
-  
-// }
-// getcontent()
 
 
 
